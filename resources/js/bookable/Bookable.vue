2fba32a11f7aea96b82682e8566a92a21e229875
https://gitlab.com/asgprv <asyljan.gapurov@gmail.com>
2020-10-08 21:39:32 +0600

    Bookable page and etc

--- resources/js/bookable/Bookable.vue
+++ resources/js/bookable/Bookable.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="row">
-        <div class="col-md-8">
+        <div class="col-md-8 pb-4">
             <div class="card">
                 <div class="card-body">
                     <div v-if="!loading">
@@ -11,16 +11,27 @@
                     <div v-else>Loading...</div>
                 </div>
             </div>
+            <review-list></review-list>
+
         </div>
-        <div class="col-md-4">
-            availablitiy & price
+        <div class="col-md-4 pb-4">
+            <availablitiy></availablitiy>
+        </div>
+        <div>
+
         </div>
         
     </div>
 </template>
 
 <script>
+import Availablitiy from './Availability'
+import ReviewList from './ReviewList'
 export default {
+    components: {
+        Availablitiy,
+        ReviewList
+    },
     data () {
         return {
             bookable: null,
